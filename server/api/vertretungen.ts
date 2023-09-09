e584a16c9a51ad300f434ea5e83463fb6e8e7fe5
Tim <timthiede1@gmail.com>
2023-09-09 23:55:06 +0200

    feat(vplan): api now shows vertretungen * previously only showed all other stuff (news, etc.)

--- server/api/vertretungen.ts
+++ server/api/vertretungen.ts
@@ -14,7 +14,7 @@ export default defineEventHandler(async (event) => {
     if (!/^[a-z0-9]{26}$/.test(req.headers.authorization)) return setErrorResponse(res, 400, "'authorization' header invalid");
 
     try {
-        const raw = await fetch("http://localhost/vertretungsplan.html", {
+        const raw = await fetch("https://start.schulportal.hessen.de/vertretungsplan.php", {
             headers: {
                 Cookie: `sid=${encodeURIComponent(req.headers.authorization)}`,
                 ...generateDefaultHeaders(address)
@@ -30,7 +30,7 @@ export default defineEventHandler(async (event) => {
         };
 
         const { window } = new JSDOM(removeBreaks(await raw.text()));
-        const days = window.document.querySelectorAll("#content .panel");
+        const days = window.document.querySelectorAll("#content .panel.panel-info:not(#menue_tag)");
 
         for (const day of days) {
             const date = day.id.replace(/tag/i, "").split("_");
@@ -47,11 +47,44 @@ export default defineEventHandler(async (event) => {
                     .replace(/ <br> /gi, "<br>")
                     .replace(/(<img)[^>]*(>)/gi, "")
             );
+            // We can assume that the table has the attribute classview, for
+            // whatever that may be used for, it is most definetly present (hopefully lol)
+            const table = day.querySelector("table.table[data-classview]");
+            if (table === null) continue;
+            const vertretungen: Vertretung[] = [];
+            table.querySelectorAll("tbody > tr").forEach((entry) => {
+                
+                const children = Array.from(entry.children);
+
+                // This has to be done as there might be things like 1 - 10 in lessons
+                // which, if we would only convert the two to an array would result in
+                // weird behaviour we might not want to have. However we still just
+                // give the basic from-to back to the user in the API as seen below
+                const fromTo = children[1].innerHTML.trim().split(" - ").map((lesson) => parseInt(lesson));
+                const lessons = [];
+                for (let i = fromTo[0]; i < fromTo[1] + 1; i++) lessons.push(i);
+
+                vertretungen.push({
+                    lessons: {
+                        list: lessons,
+                        from: fromTo[0],
+                        to: fromTo[1]
+                    },
+                    class: children[2].innerHTML.trim() || null,
+                    substitute: children[3].innerHTML.trim() || null,
+                    teacher: children[4].innerHTML.trim() || null,
+                    subject: children[5].innerHTML.trim() || null,
+                    subject_old: children[6].innerHTML.trim() || null,
+                    room: children[7].innerHTML.trim() || null,
+                    note: children[8].innerHTML.trim() || null
+                });
+
+            });
             response.days.push({
-                date: date.join("."),
+                date: time,
                 day_of_week: DAYS[time.getDay()],
                 relative: headerElements[0]?.textContent || "",
-                vertretungen: [],
+                vertretungen,
                 news
             });
         }
@@ -64,12 +97,16 @@ export default defineEventHandler(async (event) => {
 });
 
 interface Vertretung {
-    lesson: string;
-    class: string;
-    substitute: string;
-    teacher: string;
-    subject: string;
-    subject_old: string;
-    room: string;
-    note: string;
+    lessons: {
+        list: number[];
+        from: number;
+        to: number;
+    };
+    class: string | null;
+    substitute: string | null;
+    teacher: string | null;
+    subject: string | null;
+    subject_old: string | null;
+    room: string | null;
+    note: string | null;
 }
